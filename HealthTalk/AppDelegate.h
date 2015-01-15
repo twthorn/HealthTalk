a4620bc8dbd1e3f4bf9b9eaf5f61c9fc1019a70b
Thomas Thornton <thomasthornton@Thomass-MacBook-Pro-3.local>
2015-01-14 21:48:10 -0800

    Initial Commit

---
+++ HealthTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthTalk
+//
+//  Created by Thomas Thornton on 1/14/15.
+//  Copyright (c) 2015 ThomasApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthTalk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HealthTalk
+//
+//  Created by Thomas Thornton on 1/14/15.
+//  Copyright (c) 2015 ThomasApps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
